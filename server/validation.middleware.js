0942896465052aa35b4833d84165718e982e71ec
Beatrice Schembri <beatrice.schembri@gmail.com>
2023-05-03 15:09:45 +0200

    Fix return in validation

--- server/validation.middleware.js
+++ server/validation.middleware.js
@@ -5,6 +5,7 @@ const validation = (req, res, next) => {
   if (!title || !description || !price_in_EUR) {
     let err = new Error('Title, description and price cannot be empty');
     res.status(400).send(err.message);
+    return;
   }
   next();
 };
