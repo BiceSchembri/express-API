a07f2bc6f26b6b7ea3f003bf60a7d2b09dffcb02
Beatrice Schembri <beatrice.schembri@gmail.com>
2023-05-09 15:24:44 +0200

    Fix naming

---
+++ server/routes/post-routes.js
@@ -0,0 +1,23 @@
+// Core modules
+const express = require('express');
+const router = express.Router();
+const postController = require('../controllers/postController');
+const postValidation = require('../middlewares/postValidation');
+
+// Show all posts (no auth)
+router
+  .route('/posts')
+  .get(postController.getAll)
+  // create post (user auth)
+  .post(postValidation, postController.create);
+
+// Show, update, delete single post
+// add user auth
+router
+  .route('/posts/:id')
+  .get(postNotFound, postController.getOne)
+  .put(postNotFound, postValidation, postController.update)
+  .delete(postNotFound, postController.delete);
+
+// Export the router
+module.exports = router;
