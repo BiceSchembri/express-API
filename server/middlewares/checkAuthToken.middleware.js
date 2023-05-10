9754a8a57987edf02effb39ea7af0278c955a9d4
Beatrice Schembri <beatrice.schembri@gmail.com>
2023-05-10 12:23:29 +0200

    Fix auth middleware

--- server/middlewares/checkAuthToken.middleware.js
+++ server/middlewares/checkAuthToken.middleware.js
@@ -1,29 +1,6 @@
 const jwt = require('jsonwebtoken');
 const jwt_token = process.env.JWT_ACCESS_TOKEN;
 
-// const checkAuthToken = (req, res, next) => {
-//   const authHeader = req.headers.authorization;
-//   const token = authHeader && authHeader.split(' ')[1];
-
-//   if (!token) {
-//     return res.status(401).send('Lacking credentials');
-//   }
-//   try {
-//     jwt.verify(token, jwt_token, (err, user) => {
-//       if (err)
-//         return res
-//           .status(403)
-//           .send('You are not authorized to perform this request');
-//       req.user = user;
-//       next();
-//     });
-//   } catch (err) {
-//     res.status(401).send('this is not authorized');
-//   }
-// };
-
-const cookieParser = require('cookie-parser');
-
 const checkAuthToken = (req, res, next) => {
   //extract the access token from the request cookie
   const accessToken = req.cookies['tattoo_eshop.process'];
