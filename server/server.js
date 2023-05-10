568de23b96dfd681c4001ab993d9da090e47b844
Beatrice Schembri <beatrice.schembri@gmail.com>
2023-05-10 10:55:43 +0200

    Add cookieParser

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 // const path = require('path');
 const cors = require('cors');
 const dotenv = require('dotenv');
-// const cookies = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 
 // Import routers
 const productRouter = require('./routes/product-routes');
@@ -19,7 +19,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 dotenv.config();
-// app.use(cookies);
+app.use(cookieParser());
 
 // Set PORT and HOST from .env file, with hard-coded alternative
 const port = process.env.PORT;
